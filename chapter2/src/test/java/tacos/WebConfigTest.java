3dce012a58edb2bdd68251ebe5fbc53e2c9e22ac
Hugo <Ictyospore@protonmail.com>
2023-10-04 18:05:08 +0200

    Clean JUnit

--- chapter2/src/test/java/tacos/WebConfigTest.java
+++ chapter2/src/test/java/tacos/WebConfigTest.java
@@ -9,13 +9,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 
 @RunWith(SpringRunner.class)
-public class HomeControllerTest {
+public class WebConfigTest {
 
 	@Autowired
 	private MockMvc mockMvc;
